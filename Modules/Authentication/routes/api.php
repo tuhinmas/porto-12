6da889f0cf67f758ae1dcb1b7da8ec28af34b0a2
mastuhin <mastuhin33@gmail.com>
2025-05-25 22:56:42 +0700

    auth: login setup

--- Modules/Authentication/routes/api.php
+++ Modules/Authentication/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\Authentication\Http\Controllers\AuthenticationController;
 
 Route::group([
-    'middleware' => 'api',
+    'middleware' => 'auth',
     'prefix'     => 'auth',
 ], function ($router) {
     Route::post('login', [AuthenticationController::class, 'login']);
